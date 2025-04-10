b576869e3519ad2c69635024200c567b02705e73
Shaquille Rashad Mandy <shaqmandy@gmail.com>
2025-04-10 00:13:56 -0300

    fixed styling issue with layout and user info, theme toggler and sign out button

--- src/layouts/sidebar-layout/sidebar-user-info-and-features.tsx
+++ src/layouts/sidebar-layout/sidebar-user-info-and-features.tsx
@@ -29,34 +29,55 @@ export default function SidebarUserInfoAndFeatures(props: SidebarUserInfoAndFeat
         await handleServerResponse({ redirectTo: routes.auth.signOut, response, toast })
     }, [])
 
+    const LogoutButton = (
+        <TooltipProvider delayDuration={0}>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button className="h-8 w-8" onClick={handleSignOut} variant="ghost">
+                        <LogOut className="h-4 w-4" />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Logout</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+
+    const UserAvatar = (
+        <Avatar className="h-8 w-8">
+            <AvatarImage alt="Avatar" src={user?.imageUrl ?? ""} />
+            <AvatarFallback>
+                {user?.firstName?.charAt(0)}
+                {user?.lastName?.charAt(0)}
+            </AvatarFallback>
+        </Avatar>
+    )
+
     return (
         <div className="flex flex-col items-center gap-2 p-4 group-data-[collapsible=icon]:flex-col group-data-[collapsible=icon]:justify-center">
-            <div className="flex items-center gap-2">
+            {/* Normal view: Theme > Avatar > Logout */}
+            <div className="flex items-center gap-2 group-data-[collapsible=icon]:hidden">
                 {/* Theme Mode Toggle */}
                 <ThemeModeToggler />
 
                 {/* User avatar */}
-                <Avatar className="h-8 w-8">
-                    <AvatarImage alt="Avatar" src={user?.imageUrl ?? ""} />
-                    <AvatarFallback>
-                        {user?.firstName?.charAt(0)}
-                        {user?.lastName?.charAt(0)}
-                    </AvatarFallback>
-                </Avatar>
+                {UserAvatar}
 
                 {/* Logout button */}
-                <TooltipProvider delayDuration={0}>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button className="h-8 w-8" onClick={handleSignOut} variant="ghost">
-                                <LogOut className="h-4 w-4" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Logout</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
+                {LogoutButton}
+            </div>
+
+            {/* Collapsed view: Theme > Logout > Avatar */}
+            <div className="hidden items-center gap-2 group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:flex-col">
+                {/* Theme Mode Toggle */}
+                <ThemeModeToggler />
+
+                {/* Logout button before avatar when collapsed */}
+                {LogoutButton}
+
+                {/* User avatar */}
+                {UserAvatar}
             </div>
 
             {/* User info */}
