6cc9958523d3f6b991e3c2984c1d3a3492d9cb3a
Shaquille Rashad Mandy <shaqmandy@gmail.com>
2025-04-27 05:03:19 -0300

    updated css for date range selector

--- src/views/home/blocks/analytics/analytics.tsx
+++ src/views/home/blocks/analytics/analytics.tsx
@@ -65,10 +65,9 @@ export default function Analytics(props: AnalyticsProps) {
     return (
         <div className="min-h-screen bg-background">
             <div className="sticky top-0 z-10 border-b bg-background pb-2 pt-4">
-                <div className="container mx-auto px-4 md:px-6">
-                    <DateRangeSelector dateRange={dateRange} setDateRange={setDateRange} />
-                </div>
+                <DateRangeSelector dateRange={dateRange} setDateRange={setDateRange} />
             </div>
+
             <div className="mx-auto space-y-4 py-6">
                 <div className="mb-6 grid gap-6 overflow-x-auto md:grid-cols-4">
                     <FinanceCard amount={getTotalIncome()} subTitle="Total Money Coming In" title="Total Income" />
